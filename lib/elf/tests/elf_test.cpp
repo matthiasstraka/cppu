7b77ecf51f98c09e85c89077b7d6f8a3f824e0ff
Matthias Straka <mstraka84@gmail.com>
2025-11-18 22:21:49 +0100

    elf: move simple file to examples folder

--- lib/elf/tests/elf_test.cpp
+++ lib/elf/tests/elf_test.cpp
@@ -8,7 +8,7 @@ BOOST_AUTO_TEST_SUITE(elf_suite)
 
 BOOST_AUTO_TEST_CASE(default_test)
 {
-    ElfFile64 file(TEST_FILES_PATH "simple.bin");
+    ElfFile64 file(TEST_FILES_PATH "simple/simple.bin");
     BOOST_REQUIRE_EQUAL(file.getNumProgramSections(), 11);
     BOOST_CHECK_EQUAL(file.getString(1), ".symtab");
 }
